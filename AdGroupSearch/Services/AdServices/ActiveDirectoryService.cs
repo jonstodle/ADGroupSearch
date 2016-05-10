1cacabb31f75787c6eda7715e13fb8192976a779
Jon Stødle <jonstodle@outlook.com>
2016-05-10 16:25:53 +0200

    Day 1

---
+++ AdGroupSearch/Services/AdServices/ActiveDirectoryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdGroupSearch.Services.AdServices
+{
+    public class ActiveDirectoryService
+    {
+    }
+}
---
+++ AdGroupSearch/ViewModels/MainWindowModel.cs
@@ -0,0 +1,48 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdGroupSearch.ViewModels
+{
+    public class MainWindowModel : ReactiveObject
+    {
+        public ReactiveList<GroupPrincipal> Groups { get; set; }
+
+        public ReactiveCommand<object> LoadGroups { get; set; }
+
+        private string filterText;
+        public string FilterText
+        {
+            get { return filterText; }
+            set { this.RaiseAndSetIfChanged(ref filterText, value); }
+        }
+
+        public ReactiveCommand<object> FilterGroups { get; set; }
+
+
+
+        public MainWindowModel()
+        {
+            Groups = new ReactiveList<GroupPrincipal>();
+
+
+
+            LoadGroups = ReactiveCommand.Create();
+
+
+
+            FilterGroups = null;
+        }
+
+
+
+        private void LoadGroupsImpl()
+        {
+
+        }
+    }
+}
