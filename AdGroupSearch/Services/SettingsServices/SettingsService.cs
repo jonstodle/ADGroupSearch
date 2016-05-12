f22a06f8dd56e0bf50c5a0a9c050e7ffe5618805
Jon Stødle <jonstodle@outlook.com>
2016-05-12 15:42:57 +0200

    Add settings service

---
+++ AdGroupSearch/Services/SettingsServices/SettingsService.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdGroupSearch.Services.SettingsServices
+{
+    public class SettingsService
+    {
+        [JsonIgnore]
+        public static SettingsService Current { get; private set; }
+        static SettingsService() { Current = Current ?? new SettingsService(); }
+
+
+
+        SettingsService()
+        {
+
+        }
+    }
+}
