51ba9ee78ed641404f9d436f7b9594d88b72b04e
Jon Stødle <jonstodle@outlook.com>
2017-02-20 12:00:40 +0100

    Clean up UI markup and move bindings to RxUI bindings

--- AdGroupSearch/MainWindow.xaml.cs
+++ AdGroupSearch/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using AdGroupSearch.ViewModels;
+using ReactiveUI;
 using System;
+using System.Reactive;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Windows;
 
@@ -8,33 +11,44 @@ namespace AdGroupSearch
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IViewFor<MainWindowViewModel>
     {
         public MainWindow()
         {
             InitializeComponent();
 
-            Observable.FromEventPattern(this, nameof(MainWindow.Activated))
-                .Skip(1)
-                .Subscribe(_ => ViewModel.CheckCache.Execute(null));
+            ViewModel = new MainWindowViewModel();
 
-            Observable.FromEventPattern(this, nameof(MainWindow.Activated))
-                .Delay(TimeSpan.FromMilliseconds(100))
-                .ObserveOnDispatcher()
-                .Subscribe(x =>
-                {
-                    FilterTextBox.Focus();
-                    FilterTextBox.SelectAll();
-                });
+            this.WhenActivated(disposables =>
+            {
+                this.Bind(ViewModel, vm => vm.FilterText, v => v.FilterTextBox.Text).DisposeWith(disposables);
+                this.Bind(ViewModel, vm => vm.UseFuzzy, v => v.UseFuzzyToggleButton.IsChecked).DisposeWith(disposables);
+                this.OneWayBind(ViewModel, vm => vm.Groups, v => v.GroupsListView.ItemsSource).DisposeWith(disposables);
+                this.Bind(ViewModel, vm => vm.SelectedGroup, v => v.GroupsListView.SelectedItem).DisposeWith(disposables);
+                this.OneWayBind(ViewModel, vm => vm.IsExecutingLoadGroups, v => v.LoadingIndicatorBorder.Visibility).DisposeWith(disposables);
+                this.OneWayBind(ViewModel, vm => vm.Groups.Count, v => v.TotalCountRun.Text).DisposeWith(disposables);
 
-            Observable.FromEventPattern<RoutedEventArgs>(MenuButton, nameof(Button.Click))
-                .Subscribe(e =>
-                {
-                    MenuButtonContextMenu.PlacementTarget = e.Sender as Button;
-                    MenuButtonContextMenu.IsOpen = true;
-                });
+                this.BindCommand(ViewModel, vm => vm.LoadGroups, v => v.RefreshButton).DisposeWith(disposables);
+
+                Observable.Return(Unit.Default)
+                    .InvokeCommand(ViewModel.LoadGroups)
+                    .DisposeWith(disposables);
+
+                Observable.Return(Unit.Default)
+                    .Delay(TimeSpan.FromMilliseconds(100))
+                    .ObserveOnDispatcher()
+                    .Subscribe(x =>
+                    {
+                        FilterTextBox.Focus();
+                        FilterTextBox.SelectAll();
+                    })
+                    .DisposeWith(disposables);
+            });
         }
 
-        MainWindowModel ViewModel => (MainWindowModel)DataContext;
+        public MainWindowViewModel ViewModel { get => (MainWindowViewModel)GetValue(ViewModelProperty); set => SetValue(ViewModelProperty, value); }
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(nameof(ViewModel), typeof(MainWindowViewModel), typeof(MainWindow), new PropertyMetadata(null));
+
+        object IViewFor.ViewModel { get => ViewModel; set => ViewModel = value as MainWindowViewModel; }
     }
 }
